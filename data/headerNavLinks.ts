11175ef5386293608211e992cb42881cbb3b219e
ioan-gwenter <ioangwenterwork@gmail.com>
2024-08-08 14:54:58 +0100

    initialize website

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/about', title: 'Committee' },
+]
+
+export default headerNavLinks
